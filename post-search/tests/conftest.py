e52629b7d153062989805d9f7425aa7e3b2e86a0
Tnirpps <egor.tkachenko.97@mail.ru>
2025-04-13 06:49:00 +0300

    update service

--- post-search/tests/conftest.py
+++ post-search/tests/conftest.py
@@ -1,28 +1,21 @@
 import pathlib
-import sys 
+import sys
 
-import handlers.hello_pb2_grpc as hello_services 
+import post_search_pb2_grpc as post_search_services
 import pytest
 
 
 pytest_plugins = [
     'pytest_userver.plugins.core',
-    'pytest_userver.plugins.grpc', 
+    'pytest_userver.plugins.grpc',
 ]
-USERVER_CONFIG_HOOKS = [
-    # 'prepare_service_config_grpc', 
-]
-
-
+USERVER_CONFIG_HOOKS = []
 
 
 @pytest.fixture
 def grpc_service(grpc_channel, service_client):
-    return hello_services.HelloServiceStub(grpc_channel)
+    return post_search_services.ProtoConverterStub(grpc_channel)
 
 
 def pytest_configure(config):
-    sys.path.append(str(pathlib.Path(__file__).parent.parent / 'proto/handlers/'))
-
-
-
+    sys.path.append(str(pathlib.Path(__file__).parent.parent / 'proto/'))
